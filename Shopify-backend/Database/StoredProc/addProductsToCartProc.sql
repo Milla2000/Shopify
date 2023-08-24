74a8376b50e0dd54adfccc3f99fa836db932515b
Milla2000 <millajesso2000@gmail.com>
2023-08-24 14:16:59 +0300

    slight changes

--- Shopify-backend/Database/StoredProc/addProductsToCartProc.sql
+++ Shopify-backend/Database/StoredProc/addProductsToCartProc.sql
@@ -9,12 +9,15 @@ BEGIN
     -- Add the product to the cartItemsTable
     INSERT INTO cartItemsTable (id, cart_id, product_id, product_name, price)
     VALUES (@id, @cart_id, @product_id, @product_name, @price);
-
-    -- Update the num_items in productsTable
-    UPDATE productsTable SET num_items = num_items - 1 WHERE id = @product_id;
-
+       
     -- Calculate total price for the user's cart
     SELECT SUM(price) AS total_price FROM cartItemsTable WHERE cart_id = @cart_id;
+
+    -- Update the num_items in productsTable
+    -- UPDATE productsTable SET num_items = num_items - 1 WHERE id = @product_id;
+
+   
+
 END;
 
 
