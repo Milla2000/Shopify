d24808bcafbd499fb9232ed51d45b9081ab4e1eb
Ngatia Mwai <ngatia.mwai@thejitu.com>
2023-08-27 23:42:05 +0300

    Consumed some Endpoints

---
+++ Frontend/cartProducts.js
@@ -0,0 +1,78 @@
+function cartItems() {
+    const token = localStorage.token;
+
+    axios
+        .get(
+            "http://localhost:4500/users/allcartitems",
+            {
+                headers: {
+                    "Accept": "application/json",
+                    "Content-type": "application/json",
+                    "token": token
+                },
+            }
+        )
+        .then((res) => {
+            const result = res.data;
+            console.log(result);
+            
+            const tableBody = document.querySelector('.cart-table'); // Select the table body element
+
+            result.cartItems.forEach(cartItem => {
+                
+                const newRow = document.createElement('tr');
+                newRow.innerHTML = `
+                    <td>${cartItem.id}</td>
+                    <td>${cartItem.user_id}</td>
+                    <td>${cartItem.phone_number}</td>
+                    <td>
+                        <div class="actions">
+                            <button class="button remove-button" data-id="${cartItem.id}" style="background-color: rgba(203, 34, 34, 1);">Remove</button>
+                        </div>
+                    </td>
+                `;
+
+                tableBody.appendChild(newRow); // Append the new row to the table body
+            });
+
+            // Add event listener to remove and edit buttons
+            tableBody.addEventListener('click', (event) => {
+                function deleteUser(userId) {
+                    const token = localStorage.token;
+                
+                    axios.delete(`http://localhost:4500/users/softdelete/${userId}`, {
+                        headers: {
+                            "Accept": "application/json",
+                            "Content-type": "application/json",
+                            "token": token
+                        }
+                    })
+                    .then((res) => {
+                        console.log("User deleted:", res.data);
+                        alert("User deleted successfully!");
+                        // You might want to refresh the user list after deletion
+                        users();
+                    })
+                    .catch((error) => {
+                        console.error('An error occurred:', error);
+                    });
+                }
+                
+                if (event.target.classList.contains('remove-button')) {
+                    const id = event.target.getAttribute('data-id');
+                    if (confirm('Are you sure you want to delete this user?')) {
+                        deleteUser(id);
+                    }
+                } else if (event.target.classList.contains('edit-button')) {
+                    const id = event.target.getAttribute('data-id');
+                    editUser(id);
+                }
+            });
+        })
+        .catch((error) => {
+            console.error('An error occurred:', error);
+        });
+}
+
+// Call the function to fetch and display cart items
+cartItems();
--- Frontend/userProfile.js
+++ Frontend/userProfile.js
@@ -1,28 +1,35 @@
-function editProduct(id) {
+function editUserProfile() {
     const token = localStorage.token;
     const editForm = document.createElement('div');
     editForm.innerHTML = `
-    <form id="editProfileForm">
-    <label for="editName">Name:</label>
-    <input type="text" id="editName" name="name">
-    <label for="editEmail">Email:</label>
-    <input type="email" id="editEmail" name="email">
-    <label for="editPhone">Phone:</label>
-    <input type="number" id="editPhone" name="Phone">
-    <label for="editPassword">Password:</label>
-    <input type="text" id="editPassword" name="Password">
-    <button type="submit">Save Changes</button>
-</form>
+        <form id="editProfileForm">
+            <label for="editName">Name:</label>
+            <input type="text" id="editName" name="name">
+            <label for="editEmail">Email:</label>
+            <input type="email" id="editEmail" name="email">
+            <label for="editPhone">Phone:</label>
+            <input type="number" id="editPhone" name="Phone">
+            <label for="editPassword">Password:</label>
+            <input type="text" id="editPassword" name="Password">
+            <button type="submit">Save Changes</button>
+        </form>
     `;
 
-    const productDetails = editForm.querySelector('#editProductForm');
-    productDetails.addEventListener('submit', (e) => {
+    // Append the edit form to a container in your HTML
+    const editContainer = document.querySelector('.edit-container');
+    editContainer.innerHTML = ''; // Clear previous contents
+    editContainer.appendChild(editForm);
+
+    const editProfileForm = editForm.querySelector('#editProfileForm');
+    editProfileForm.addEventListener('submit', (e) => {
         e.preventDefault();
 
-        const editedName = productDetails.querySelector('#editName').value;
-        const editedEmail = productDetails.querySelector('#editEmail').value;
-        const editedPhone = productDetails.querySelector('#editPhone').value;
-        const editedPassword = productDetails.querySelector('#editPassword').value;
+        const editedName = editProfileForm.querySelector('#editName').value;
+        const editedEmail = editProfileForm.querySelector('#editEmail').value;
+        const editedPhone = editProfileForm.querySelector('#editPhone').value;
+        const editedPassword = editProfileForm.querySelector('#editPassword').value;
+
+        const id = localStorage.getItem('id'); // Get the user ID from local storage
 
         axios.put(
             `http://localhost:4500/users/${id}`,
@@ -41,17 +48,15 @@ function editProduct(id) {
             }
         )
         .then((res) => {
-            console.log("Product updated:", res.data);
-            alert("Profile updated successfully!");
-            // products(); // Refresh the product list after update
+            console.log("Profile updated:", res.data);
+            const successMessage = document.querySelector('.success-message');
+            successMessage.textContent = res.data.message; // Update the success message content
         })
         .catch((error) => {
             console.error('An error occurred:', error);
         });
     });
-
-    // Append the edit form to a container in your HTML
-    const editContainer = document.querySelector('.edit-container');
-    editContainer.innerHTML = ''; // Clear previous contents
-    editContainer.appendChild(editForm);
 }
+
+// Call the function to set up the edit form
+editUserProfile();
