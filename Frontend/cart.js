3fa45c4b0d1ce63066838428cfef910857847fa7
Milla2000 <millajesso2000@gmail.com>
2023-08-27 19:57:58 +0300

    integrated cart item table

--- Frontend/cart.js
+++ Frontend/cart.js
@@ -1,17 +1,38 @@
-function cart(){
-    const id = localStorage.id
-    const token = localStorage.token
+// cart.js
 
+function cart() {
+    const token = localStorage.token;
+    const userId = localStorage.id;
 
-    axios
-        .get(
-            `http://localhost:4500/cart/cart-items`,
-            {
-                headers: {
-                    "Accept": "application/json",
-                    "Content-type": "application/json",
-                    "token": token
-                },
-            }
-        )
+    fetch(`http://localhost:4500/cart/cart-items/${userId}`, {
+        method: "GET",
+        headers: {
+            "Accept": "application/json",
+            "Content-type": "application/json",
+            "token": token
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            const cartTable = document.getElementById("cart-table");
+
+            // Clear existing table rows
+            cartTable.innerHTML = "";
+
+            // Populate the table with cart items
+            data.cartItems.forEach(item => {
+                const row = document.createElement("tr");
+                row.innerHTML = `
+                <td>${item.product_name}</td>
+                <td>Ksh ${item.price}</td>
+            `;
+                cartTable.appendChild(row);
+            });
+        })
+        .catch(error => {
+            console.error("Error fetching cart items:", error);
+        });
 }
+
+// Call the cart function when the page loads
+cart();
--- Frontend/yourProducts.js
+++ Frontend/yourProducts.js
@@ -1,48 +1,89 @@
-function yourProducts() {
-    const token = localStorage.token;
-    
-    axios
-        .get(
-            "http://localhost:4500/products",
-            {
-                headers: {
-                    "Accept": "application/json",
-                    "Content-type": "application/json",
-                    "token": token
-                },
-            }
-        )
+const token = localStorage.token;
+const user_id = localStorage.id;
+
+const addToCart = async (product_id) => {
+    const response = await fetch("http://localhost:4500/cart/add-to-cart", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "token": token
+        },
+        body: JSON.stringify({user_id, product_id})
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+        alert("Product added to cart successfully");
+    } else {
+        alert(data.error);
+    }
+};
+
+
+const yourProducts = () => {
+    axios.get("http://localhost:4500/products", {
+        headers: {
+            "Accept": "application/json",
+            "Content-type": "application/json",
+            "token": token
+        },
+    })
         .then((res) => {
             const result = res.data;
             console.log(result);
 
-            const yourProductsBody = document.querySelector('.actual-products'); // Select the container div
+            const yourProductsBody = document.getElementById('actual-products');
 
             result.products.forEach(product => {
-                const productContainer = document.createElement('div'); 
-                productContainer.classList.add('product-item'); 
-                
+                const productContainer = document.createElement('div');
+                productContainer.classList.add('product-item');
+
                 productContainer.innerHTML = `
-                    <div class="productImage">
-                        <img src="${product.image}" alt="" >
-                    </div>
-                    <p>${product.name}</p>
-                    <p>Ksh ${product.price}</p>
-                    <img src="./Images/stars.png" alt="" style="height: 3vh; width: 16vh;">
-                    <button class="addToCart">
-                        <a href="./productInfo.html?id=${product.id}&price=${product.price}&image="${product.image}"&description=${encodeURIComponent(product.description)}&name=${encodeURIComponent(product.name)}" style="color: #fff;">Add to cart</a>
-                    </button>
+                <div class="productImage">
+                    <img src="${product.image}" alt="">
+                </div>
+                <p>${product.name}</p>
+                <p>Ksh ${product.price}</p>
+                <img src="./Images/stars.png" alt="" style="height: 3vh; width: 16vh;">
+                 
+                <button class="addToCart" data-product-id="${product.id}" style="color: #fff;">Add to cart</button>
+            `;
 
-                `;
+                productContainer.querySelector('.addToCart').addEventListener("click", () => {
+                    addToCart(product.id);
+                });
 
-                yourProductsBody.appendChild(productContainer); // Append the product container
+                yourProductsBody.appendChild(productContainer);
             });
         })
         .catch((error) => {
             console.error('An error occurred:', error);
         });
+};
+
+function myFunction() {
+    // Declare variables
+    var input, filter, ul, productItems, p, i, txtValue;
+    input = document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    ul = document.querySelector('.actual-products');
+    productItems = ul.getElementsByClassName('product-item');
+
+    for (i = 0; i < productItems.length; i++) {
+        p = productItems[i].querySelector("p:nth-child(2)"); // Select the second <p> element within .product-item
+        txtValue = p.textContent || p.innerText;
+
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            productItems[i].classList.add('show');
+            productItems[i].classList.remove('hide');
+        } else {
+            productItems[i].classList.remove('show');
+            productItems[i].classList.add('hide');
+        }
+    }
 }
 
-yourProducts();
 
-  
+document.addEventListener("DOMContentLoaded", () => {
+    yourProducts();
+});
